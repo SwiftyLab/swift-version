78514fdd4ded4488bea86d23cd62ea451683a0f7
Soumya Ranjan Mahunt <soumya.mahunt@gmail.com>
2022-06-18 21:57:47 +0530

    refactor: reduce methods cognitive complexity

--- src/swiftorg.js
+++ src/swiftorg.js
@@ -11,27 +11,18 @@ const buildDir = 'swiftorg/_data/builds';
 const betaVerRegex = /latest-(dev|beta)/;
 const swiftReleaseRegex = /swift-.*-release/;
 
-async function buildData(version, includeDev) {
-  const ver = semver.coerce(version) ? version : null;
-  const buildDirPattern = ver ? `swift-${ver.replaceAll('.', '_')}*` : '*';
-
-  let files = await glob(`${buildDir}/${buildDirPattern}/${platform.file}.yml`);
+function filterReleaseFiles(files, version, includeDev) {
   if (!includeDev && !version.match(betaVerRegex)) {
-    files = files.filter(file => path.basename(path.dirname(file)).match(swiftReleaseRegex));
+    return files.filter(file => path.basename(path.dirname(file)).match(swiftReleaseRegex));
   }
+  return files;
+}
 
-  if (files.length) {
-    const futureDatas = files.map(file => readFile(path.join(process.cwd(), file), 'utf-8'));
-    const datas = await Promise.all(futureDatas);
-    return datas.flatMap(data => yaml.load(data));
-  }
-
+async function platformFallbackVersionBuildData(buildDirPattern, version, includeDev) {
   const regex = new RegExp(`${platform.name}(?<version>[0-9]*)(-.*)?`);
   let maxVer, minVer;
-  files = await glob(`${buildDir}/${buildDirPattern}/${platform.filePattern}.yml`);
-  if (!includeDev && !version.match(betaVerRegex)) {
-    files = files.filter(file => path.basename(path.dirname(file)).match(swiftReleaseRegex));
-  }
+  let files = await glob(`${buildDir}/${buildDirPattern}/${platform.filePattern}.yml`);
+  files = filterReleaseFiles(files, version, includeDev);
   files.forEach(file => {
     const filename = path.basename(file, 'yml');
     const ver = parseInt(filename.match(regex).groups.version);
@@ -51,6 +42,22 @@ async function buildData(version, includeDev) {
     return datas.flatMap(data => yaml.load(data));
   }
   return [];
+}
+
+async function buildData(version, includeDev) {
+  const ver = semver.coerce(version) ? version : null;
+  const buildDirPattern = ver ? `swift-${ver.replaceAll('.', '_')}*` : '*';
+
+  let files = await glob(`${buildDir}/${buildDirPattern}/${platform.file}.yml`);
+  files = filterReleaseFiles(files, version, includeDev);
+
+  if (files.length) {
+    const futureDatas = files.map(file => readFile(path.join(process.cwd(), file), 'utf-8'));
+    const datas = await Promise.all(futureDatas);
+    return datas.flatMap(data => yaml.load(data));
+  }
+
+  return platformFallbackVersionBuildData(buildDirPattern, version, includeDev);
 }
 
 module.exports = { buildData };
